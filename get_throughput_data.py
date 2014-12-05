2616e51b5d1278b1c146408f7219ee1b05d0fc89
Holly Tancredi <tancredi.holly@gmail.com>
2014-12-05 15:38:09 -0500

    Add throughput analysis script

---
+++ get_throughput_data.py
@@ -0,0 +1,78 @@
+# This script retrieves BOINC throughput data from the mySQL server
+# It prints 2 files - .dat (formatted for plotting with gnuplot) and .out (human readable output
+# Correct usage is: get_throughput_data.py <start date YYYY/MM/DD> <end date YYYY/MM/DD>
+
+from __future__ import print_function
+import calendar
+import MySQLdb
+import sys
+import time
+from datetime import datetime, date
+
+# Connect to the boinc mySQL database
+db = ""
+db_user = ""
+db_pw = ""
+db = MySQLdb.connect(user=db_user, passwd=db_pw, db=db)
+
+# Create cursor object to execute queries
+curr = db.cursor()
+
+# Results that are NOT a Success
+failed_outcomes = { 2 : 'COULDN\'T SEND' , 3 : 'COMPUTATION ERROR', 4 : 'NO REPLY', 5 : 'DIDN\'T NEED', 6 : 'VALIDATE ERROR', 7 : 'ABANDONED'}
+
+completed = 5
+success = 1
+
+# Get the date options
+start = str(sys.argv[1])
+end = str(sys.argv[2])
+start_struct = time.strptime(start, "%Y/%m/%d")
+start_date = calendar.timegm(start_struct)
+end_struct = time.strptime(end, "%Y/%m/%d")
+# Add seconds for end of day on end_date
+end_date = calendar.timegm(end_struct) + 86399
+
+# Create output files
+datafile = open('test.dat', 'a+')
+outputfile = open('test.out', 'a+')
+
+# Query the database
+get_finished_jobs = "SELECT outcome from result where server_state = '%s' and received_time <= %s and received_time >= %s" % (completed, end_date, start_date)
+curr.execute(get_finished_jobs)
+num_finished_jobs = curr.rowcount
+
+print ('BOINCIMPACT THROUGHPUT DATA: \n==========================', file = outputfile)
+print ('Completed jobs between %s and %s: %d' % (start, end, num_finished_jobs), file = outputfile)
+print ('#Date\tSuccess\tFailed', file = datafile)
+
+start_of_day = start_date
+failed_outcomes_count = [0]*6
+
+while start_of_day <= end_date:
+	num_success_jobs = 0
+	num_failed_jobs = 0
+	end_of_day = (start_of_day + 86399)
+	curr.execute("SELECT outcome from result where server_state = '%s' and received_time <= %s and received_time >= %s" % (completed, end_of_day, start_of_day))
+	for outcome in curr:
+		#outcome is returned as a tuple
+		if (outcome[0] == 1):
+			num_success_jobs += 1
+		else:
+			num_failed_jobs += 1
+			failed_outcomes_count[(outcome[0]-2)] += 1
+
+	# Formatting from epoch time to human readable time
+	print (time.strftime("%Y/%m/%d", time.gmtime(start_of_day)) + ': %d success %d failed' % (num_success_jobs, num_failed_jobs), file = outputfile)
+
+	# Print to .dat file
+	print (time.strftime("%Y/%m/%d", time.gmtime(start_of_day)) + '\t%d\t%d' % (num_success_jobs, num_failed_jobs), file = datafile)	
+	
+	start_of_day += 86400
+
+print ('\nREASONS FOR FAILURE: \n=========================', file = outputfile)
+for outcome in failed_outcomes: 
+	print ('%s: %d' % (failed_outcomes.get(outcome), failed_outcomes_count[(outcome-2)]), file = outputfile)
+
+# Disconnect from surver
+db.close()
