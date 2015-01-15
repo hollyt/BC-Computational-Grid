1f595ba51f86e620a9a571354360f8d7fad1a692
Holly Tancredi <tancredi.holly@gmail.com>
2015-01-15 15:27:07 -0500

    Update to work with new class

--- dms-file-manip/add-properties.py
+++ dms-file-manip/add-properties.py
@@ -1,9 +1,4 @@
-#! /usr/bin/python
-
-# This program retrieves the lambda and temperature from an output file and
-# adds them to a new table (properties) in each corresponding .dms file, for
-# every file in a given directory.
-# Usage: ./add-lambda-temp.py <directory>
+#!/usr/bin/python
 
 from __future__ import print_function
 import glob
@@ -12,16 +7,25 @@ import properties
 import re
 import sys
 
+def add_properties(file, property, value):
+	p = properties.properties(file, property, value)
+	p.connect()
+	
+	if (p.add_properties()):
+		return True
+	else:
+		return False
+
 def get_properties(filename):
 	temp_pattern = re.compile('Target temperature \(K\) = \d\d\d')
 	lam_pattern = re.compile('lambda \d\.\d*')
 	name_pattern = re.compile('b_\d{1,3}')
 	num_pattern = re.compile('\d{1,3}')
-	
+
 	f = open(filename, 'r')
 	text = f.read()
 	f.close()
-	
+
 	t = re.search(temp_pattern, text)
 	if t:
 		temp = (re.search('\d\d\d', t.group(0))).group(0)
@@ -37,17 +41,6 @@ def get_properties(filename):
 
 	return (name, temp, lam)
 
-def add_properties(file, props):
-	temp, lam = props
-	p = properties.properties(file, temp, lam)
-	p.connect()
-
-	if (p.add_properties()):
-		return True
-	else:
-		return False
-
-
 # Get the directory as command line argument
 repname = sys.argv[1:]
 if (len(repname) < 1):
@@ -60,18 +53,30 @@ default_path = path
 for filename in glob.glob(os.path.join(path, '*.out')):
 	# Compile receptor and ligand filenames
 	props = get_properties(filename)
-	name, props = props[0], props[1:]
+	name, temp_val, lam_val = props
 	__,__,num = name.rpartition('_')
-		
+	temp_type = 'Temperature'
+	lam_type = 'Lambda'
+	
 	rcpt = default_path + '/b_rcpt_{}.dms'.format(num)
 	lig = default_path + '/b_lig_{}.dms'.format(num)
-		
 	
-	if (add_properties(rcpt, props)):
-		print ('Success! Properties added to receptor: {}'.format(rcpt))
+	# Temp
+	if (add_properties(rcpt, temp_type, temp_val)):
+		print ('{} added to receptor: {}'.format(temp_type, rcpt))
 	else:
-		print ('Error adding properties to receptor: {}'.format(rcpt))
-	if (add_properties(lig, props)):
-		print ('Success! Properties added to ligand: {}'.format(lig))
+		print ('Error adding {} to receptor: {}'.format(temp_type, rcpt))
+	if (add_properties(lig, temp_type, temp_val)):
+		print ('{} added to ligand: {}'.format(temp_type, lig))
 	else:
-		print ('Error adding properties to ligand: {}'.format(lig))
+		print ('Error adding {} to receptor: {}'.format(temp_type, lig))
+	
+	# Lambda
+	if (add_properties(rcpt, lam_type, lam_val)):
+		print ('{} added to receptor: {}'.format(lam_type, rcpt))
+	else:
+		print ('Error adding {} to receptor: {}'.format(lam_type, rcpt))
+	if (add_properties(lig, lam_type, lam_val)):
+		print ('{} added to ligand: {}'.format(lam_type, lig))
+	else:
+		print ('Error adding {} to ligand: {}'.format(lam_type, lig))
